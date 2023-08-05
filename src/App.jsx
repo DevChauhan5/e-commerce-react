86ef759a7cddef955e13db3b33240df63d844aa2
Dev Chauhan <124146292+DevChauhan5@users.noreply.github.com>
2023-08-05 17:14:00 +0000

    routing added

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,32 @@
-import Home from './pages/Home'
+import Home from "./pages/Home";
+import CartPage from "./pages/CartPage";
+import ProductDetailsPage from "./pages/ProductDetailsPage";
+import ErrorPage from "./pages/ErrorPage";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { Provider } from "react-redux";
+import store from "./redux/store";
 
 function App() {
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Home />,
+      errorElement: <ErrorPage />,
+    },
+    {
+      path: "/product/:productID",
+      element: <ProductDetailsPage />,
+    },
+    {
+      path: "/cart",
+      element: <CartPage />,
+    },
+  ]);
   return (
-    <div>
-      <Home />
-    </div>
-  )
+    <Provider store={store}>
+      <RouterProvider router={router} />
+    </Provider>
+  );
 }
 
-export default App
+export default App;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,11 @@
+import { Link } from 'react-router-dom'
 import Navbar from '../components/Navbar'
 
 function Home() {
   return (
     <div>
       <Navbar />
+      <Link to="/cart">Cart</Link>
     </div>
   )
 }
