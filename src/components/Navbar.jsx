0d124ef08793eb4de4ae38e309924f557b537827
Dev Chauhan <124146292+DevChauhan5@users.noreply.github.com>
2023-09-26 17:07:39 +0000

    navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,28 @@
-import React from 'react'
+import React from "react";
 
 function Navbar() {
   return (
-    <div>Navbar</div>
-  )
+    <div className="p-3 flex items-center justify-around">
+      {/*Logohere*/}
+      <div className="flex items-center">
+        <img
+          className="w-14 h-14"
+          src="https://cdn-icons-png.flaticon.com/512/2331/2331966.png"
+          alt="logo"
+        />
+        <h1 className="text-2xl font-bold mx-2 text-red-400">E-Com</h1>
+      </div>
+      {/*Searchbarhere*/}
+      <div>
+        <input className="w-200px h-10 px-3 rounded-lg border-2 border-black" type="text" />
+        
+      </div>
+      {/*Menu Icons*/}
+      <div className="flex items-center">
+        Menu
+        </div>
+    </div>
+  );
 }
 
-export default Navbar
+export default Navbar;
