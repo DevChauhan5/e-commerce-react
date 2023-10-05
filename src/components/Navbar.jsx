06b49b89af09f06ebf85653d38c714e4434ce8a0
Dev Chauhan <124146292+DevChauhan5@users.noreply.github.com>
2023-10-05 04:23:57 +0000

    navbar update

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -35,7 +35,7 @@ function Navbar() {
             type="text"
           />
         </div>
-        <div className="flex items-center justify-center px-3 opacity-40">
+        <div className="flex items-center justify-center px-3 opacity-40 cursor-pointer hover:opacity-100">
           <span className="text-[12px]">All Categories</span>
           <svg
             xmlns="http://www.w3.org/2000/svg"
@@ -72,7 +72,69 @@ function Navbar() {
         </div>
       </div>
       {/*Menu Icons*/}
-      <div className="flex items-center">Menu</div>
+      <div className="flex items-center justify-center px-2 gap-5">
+        {/*Account Icon*/}
+        <div className="flex items-center">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="w-7 h-7 mx-1 "
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M15.75 6a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0zM4.501 20.118a7.5 7.5 0 0114.998 0A17.933 17.933 0 0112 21.75c-2.676 0-5.216-.584-7.499-1.632z"
+            />
+          </svg>
+          <div>
+            <span className="block text-[10px]">Sign in</span>
+            <span className="font-bold text-[14px]">Account</span>
+          </div>
+        </div>
+
+        {/*Message icon*/}
+        <div>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="w-6 h-6 ml-4"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M21.75 6.75v10.5a2.25 2.25 0 01-2.25 2.25h-15a2.25 2.25 0 01-2.25-2.25V6.75m19.5 0A2.25 2.25 0 0019.5 4.5h-15a2.25 2.25 0 00-2.25 2.25m19.5 0v.243a2.25 2.25 0 01-1.07 1.916l-7.5 4.615a2.25 2.25 0 01-2.36 0L3.32 8.91a2.25 2.25 0 01-1.07-1.916V6.75"
+            />
+          </svg>
+        </div>
+
+        {/*Heart Icon*/}
+          <div className="relative">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="w-6 h-6"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12z"
+              />
+            </svg>
+            <span className="absolute -top-1.5 -right-1.5 w-4 h-4 bg-red-500 rounded-full flex items-center justify-center text-white text-sm font-bold leading-none">5</span>
+          </div>
+
+        {/*Cart Icon*/}
+
+      </div>
     </div>
   );
 }
